8421674ee4a6afe88e03ee4f89cea666cc310b53
IRONHIDE-ALIEN2\monst <sbiswas7@hawk.iit.edu>
2018-08-03 02:43:52 +0530

    SHMatrix & opencv working together! :fire:

--- working_dir/visual_studio_files/CUDA-3DMap/CUDA-3DMap/image_op_demo.cpp
+++ working_dir/visual_studio_files/CUDA-3DMap/CUDA-3DMap/image_op_demo.cpp
@@ -2,6 +2,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "SHMatrix/SHMatrix.h"
 
 using namespace std;
 using namespace cv;
@@ -15,5 +16,27 @@ void show(Mat& im) {
 int main() {
 	Mat im;
 	im = cv::imread("img.jpg");
+
+	cublasHandle_t cublasHandle;
+	CublasSafeCall(cublasCreate_v2(&cublasHandle));
+
+	int n = im.rows * im.cols * im.channels();
+	float *x = (float *)malloc(sizeof(float) * n);
+	for (int i = 0; i < n; i++) {
+		x[i] = (float)im.data[i];
+	}
+
+
+	std::vector<int> a_shp = { im.rows, im.cols, im.channels() };
+	SHMatrix a(cublasHandle, x, a_shp, CPU);
+
+	a.Move2GPU();
+	a *= 20;
+	a.Move2CPU();
+
+	for (int i = 0; i < n; i++) {
+		im.data[i] = (uchar)a.data[i];
+	}
+
 	show(im);
 }
